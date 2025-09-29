42cfde4242f4e55d163980ffd82ccf22f87372d8
bruno <brunohgarcia@outlook.com>
2025-09-29 02:11:44 +0200

    add health check

---
+++ src/test/java/tasks/prod/HealhCheckIT.java
@@ -0,0 +1,31 @@
+package tasks.prod;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+public class HealhCheckIT {
+
+    @Test
+    public void healthCheck() throws MalformedURLException {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        WebDriver driver = new RemoteWebDriver(new URL("http://192.168.0.23:4444"), options);
+        try{
+            driver.navigate().to("http://192.168.0.23:9999/tasks");
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+            String version = driver.findElement(By.id("version")).getText();
+            Assert.assertTrue(version.startsWith("build"));
+        }
+        finally {
+            driver.quit();
+        }
+    }
+}
