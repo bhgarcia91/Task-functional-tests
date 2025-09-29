34c329f3fcdffa531ca7ebe80708754ce86634c3
bruno <brunohgarcia@outlook.com>
2025-09-29 23:12:19 +0200

    fixing failsafe plugin uses

--- src/test/java/tasks/functional/TasksTest.java
+++ src/test/java/tasks/functional/TasksTest.java
@@ -2,22 +2,14 @@ package tasks.functional;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.rules.Timeout;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
-
-import static org.openqa.selenium.remote.DesiredCapabilities.*;
 
 public class TasksTest {
 
