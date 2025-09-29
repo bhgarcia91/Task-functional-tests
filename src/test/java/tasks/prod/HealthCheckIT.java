ab436723c1aa9dbdae4051376edfcfcac31dbe96
bruno <brunohgarcia@outlook.com>
2025-09-29 23:33:54 +0200

    fixing typo on Health check

--- src/test/java/tasks/prod/HealthCheckIT.java
+++ src/test/java/tasks/prod/HealthCheckIT.java
@@ -11,7 +11,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
 
-public class HealhCheckIT {
+public class HealthCheckIT {
 
     @Test
     public void healthCheck() throws MalformedURLException {
